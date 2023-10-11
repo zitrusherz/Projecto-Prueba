4df123a4d4c0355ac5f608e20f946fc67e9f7fcb
Christopher Andrés Euribe Euribe <107970090+zitrusherz@users.noreply.github.com>
2023-10-11 00:37:41 -0300

    Update ClienteInputHandler.java

--- ClienteInputHandler.java
+++ ClienteInputHandler.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 public class ClienteInputHandler{
-    private final Control controlador = new Control();
-    private final Scanner sc = new Scanner(System.in);
 
-    public Cliente obtenerDatosCliente() {
+    public static Cliente obtenerDatosCliente() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Por favor ingrese su nombre: ");
         String nombre = sc.nextLine();
-                nombre = controlador.controlNoNull(nombre,"nombre");
+                nombre = Control.controlNoNull(nombre,"nombre");
 
         System.out.print("Por favor ingrese su apellido: ");
         String apellido = sc.nextLine();
-                apellido = controlador.controlNoNull(apellido,"apellido");
+                apellido = Control.controlNoNull(apellido,"apellido");
 
         System.out.print("Por favor ingrese su correo: ");
-        String correo = controlador.controlCustomerEmail();
+        String correo = Control.controlCustomerEmail();
 
         System.out.print("Por favor ingrese su telefono (Sin codigo de area ni el 9): ");
-        String telefono = controlador.controlCustomerPhone();
+        String telefono = Control.controlCustomerPhone();
 
         System.out.print("Por favor ingrese su run(sin digito verificador): ");
-        String run = controlador.controlRun();
+        String run = Control.controlRun();
 
         System.out.print("Por favor ingrese su digito verificador: ");
-        String dv = controlador.controlDv("dv");
-
+        String dv = Control.controlDv("dv");
+        sc.close();
         return new Cliente(nombre, apellido, correo, telefono, run, dv);
     }
 
