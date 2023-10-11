9055f7b8d69773b7452d04fb346c54f771dee372
Christopher Andrés Euribe Euribe <107970090+zitrusherz@users.noreply.github.com>
2023-10-11 00:40:55 -0300

    Update ProductoInputHandler.java

--- ProductoInputHandler.java
+++ ProductoInputHandler.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 public class ProductoInputHandler {
 
-    private final Control controlador = new Control();
-    private final Scanner sc = new Scanner(System.in);
 
-    public Producto obtenerDatosProducto() {
+
+
+    public static Producto obtenerDatosProducto() {
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Por favor ingrese el nombre del producto: ");
         String nombre = sc.nextLine();
-                nombre = controlador.controlNoNull(nombre,"nombre del producto");
+                nombre = Control.controlNoNull(nombre,"nombre del producto");
 
 
         System.out.print("Por favor ingrese el ID del producto: ");
-        String id = controlador.controlId();
+        String id = Control.controlId();
 
         System.out.println("Por favor ingrese la categoria del producto: ");
-        Enum categoria = controlador.controlCategoria();
+        Enum categoria = Control.controlCategoria();
 
         System.out.print("Por favor ingrese el precio del producto: ");
-        int precio = controlador.controlPrice();
+        int precio = Control.controlPrice();
 
         System.out.print("Por favor ingrese la cantidad de productos: ");
-        int cantidad = controlador.controlStockOfProductsInput();
-
+        int cantidad = Control.controlStockOfProductsInput();
+        sc.close();
         return new Producto(id, nombre,(Categorias) categoria, precio, cantidad);
     }
 }
