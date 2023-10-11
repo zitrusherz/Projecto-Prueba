70117f38e3d0077240b599e7a13be9cfabef9d3d
Christopher Andrés Euribe Euribe <107970090+zitrusherz@users.noreply.github.com>
2023-10-11 00:39:11 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,30 +1,33 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 public class Menu {
-    Control controlador = new Control();
-    final Catalogo catalogoNull = new Catalogo();
-    Catalogo catalogoDelMenu = controlador.controlNoNullListOfProducts(catalogoNull.getCatalogo());
-    ProductoInputHandler productoInputHandler = new ProductoInputHandler();
-    Scanner sc = new Scanner(System.in);
-    ClienteInputHandler clienteInputHandler = new ClienteInputHandler();
-    public Pedido menuCliente(){
-        ArrayList<Producto> pedidoCliente = new ArrayList<>();
-        Cliente nuevoCliente = clienteInputHandler.obtenerDatosCliente();
+
+    final static Catalogo catalogoNull = new Catalogo();
+    Catalogo catalogoDelMenu = Control.controlNoNullListOfProducts(catalogoNull.getCatalogo());
+
+
+    public static Pedido menuCliente(@NotNull Catalogo catalogoDelMenu){
+        Cliente nuevoCliente = ClienteInputHandler.obtenerDatosCliente();
+        HashMap<String, int[]> listaDeProductos = new HashMap<>();
+
         System.out.println("Bienvenido " + nuevoCliente.toString() + " a We are");
         Catalogo.getListaProductos();
         System.out.println("Por favor ingrese el numero correspondiente de los productos que desea comprar, para terminar ingrese 0");
         int terminar_elecc_products = -1;
-        int numeroProducto = controlador.controlNumOfProducts(catalogoDelMenu.size());
+        int posicionProducto = Control.controlNumOfProducts(catalogoDelMenu.size());
         while (terminar_elecc_products != 0) {
 
             System.out.print("Por favor ingrese la cantidad de productos que desea comprar: ");
-            int cantidadProductoInput = controlador.controlStockOfProductsInput();
-
-            controlador.controlStockOfProducts(cantidadProductoInput, numeroProducto - 1, catalogoDelMenu);
-            catalogoDelMenu.setCantidadProductoNueva(numeroProducto - 1, cantidadProductoInput, catalogoDelMenu.getCatalogo());
+            int cantidadProductoInput = Control.controlStockOfProductsInput();
+            Control.controlStockOfProducts(cantidadProductoInput, posicionProducto - 1, catalogoDelMenu);
+            listaDeProductos.put(catalogoDelMenu.getNombreProducto(posicionProducto - 1), new int[]{cantidadProductoInput, Catalogo.getPrecioProducto(posicionProducto - 1)});
+            catalogoDelMenu.setCantidadProductoNueva(posicionProducto - 1, cantidadProductoInput, Catalogo.getCatalogo());
             System.out.println("Si desea terminar de elegir productos ingrese 0, si desea seguir eligiendo productos ingrese 1");
-            terminar_elecc_products = controlador.controlInt("la eleccion");
-            terminar_elecc_products = controlador.controlGeneralChoices(terminar_elecc_products, 0, 1);
+            terminar_elecc_products = Control.controlInt("la eleccion");
+            terminar_elecc_products = Control.controlGeneralChoices(terminar_elecc_products, 0, 1);
             if(terminar_elecc_products == 1){Catalogo.getListaProductos();}
             System.out.println();
 
@@ -35,12 +38,12 @@ public class Menu {
     public Catalogo colaboradorMenu(){
 
         System.out.println("Por favor ingrese los datos del producto que desea agregar al catalogo:");
-        Producto nuevoProducto = productoInputHandler.obtenerDatosProducto();
+        Producto nuevoProducto = ProductoInputHandler.obtenerDatosProducto();
         Catalogo catalogo = new Catalogo(nuevoProducto);
         System.out.println("El producto ha sido agregado al catalogo" +
                             "\nSi desea agregar otro producto ingrese 1, si desea salir del programa ingrese 0");
-        int eleccion = controlador.controlInt("la eleccion");
-        eleccion = controlador.controlGeneralChoices(eleccion, 0, 1);
+        int eleccion = Control.controlInt("la eleccion");
+        eleccion = Control.controlGeneralChoices(eleccion, 0, 1);
         if(eleccion == 1) {
             colaboradorMenu();
         }
@@ -63,12 +66,12 @@ public class Menu {
                     + "Si usted es colaborador de We are ingrese 2\n"
                     + "Si desea salir del menú ingrese 0");
 
-            exit = controlador.controlMenu();
+            exit = Control.controlMenu();
 
             switch (exit) {
 
                 case 1:
-                    menuCliente();
+                    menuCliente(catalogoDelMenu);
                     break;
             //-------------------------------------------------------------------------------------------------------------------------------------------------
                 case 2:
@@ -79,7 +82,6 @@ public class Menu {
     }
     public static void main (String[]args){
             Scanner sc = new Scanner(System.in);
-            Control controlador = new Control();
             Menu menuRuning = new Menu();
             menuRuning.runMenu();
 
